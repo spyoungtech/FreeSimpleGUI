92e245bf6402dbd5d0836f240d0c4b7a2480f34f
Roman A <121314722+GameRoMan@users.noreply.github.com>
2025-07-01 22:23:55 +0100

    fix

--- FreeSimpleGUI/__init__.py
+++ FreeSimpleGUI/__init__.py
@@ -31,10 +31,8 @@ from math import fabs
 from tkinter import filedialog  # noqa
 from tkinter import ttk
 from tkinter.colorchooser import askcolor  # noqa
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Tuple
+
+from typing import Any, Dict, List, Literal, Optional, Tuple, Union
 
 from FreeSimpleGUI._utils import _error_popup_with_traceback
 from FreeSimpleGUI.elements.base import Element
